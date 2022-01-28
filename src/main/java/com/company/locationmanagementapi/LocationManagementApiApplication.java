fb865c931df233bba665c162d571464008c9b95a
Vaibhav Gote <vaibhav.gote@nuance.com>
2022-01-28 14:59:43 +0530

    First Commit

---
+++ src/main/java/com/company/locationmanagementapi/LocationManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.locationmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationManagementApiApplication.class, args);
+	}
+
+}
